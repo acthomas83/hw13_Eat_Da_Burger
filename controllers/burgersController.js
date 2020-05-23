ea6e6500b8902da5c8d8fd54dce48cbab8bdedad
Aaron Thomas <acthomas83@gmail.com>
2020-05-23 12:44:43 -0400

    push

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -7,7 +7,7 @@ let burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", (req, res) => {
-  burger.all(function(data) {
+  router.all(function(data) {
     let hbsObject = {
       burgers: data
     };
